cbc41e73449966abdc070b562f038703d9b3728c
Codewithdean <142483331+Codewithdean@users.noreply.github.com>
2023-08-21 09:13:04 +0000

    Datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all js code as newer version
+
+//alert(3+3)  we are using node not browser
+
+console.log(3
+    +3) //code reability are high
+
+//use original documentaions ACMA scripts
+
+let name="Aman"
+let age=22
+let isLoggedIn = false   // true/false
+
+//number= 2 to the power 53
+//bigint 
+//string=""
+//boolean=true/false
+//null= standlone value means empty value
+//undefined=value are not defined
+//symbol=unique
+
+
+//object
+
+console.log(typeof "Aman")
+console.log(typeof null);  // if run the typeof of null the value given is object. this is the important part of the interview process
+console.log(typeof undefined);

6f61750302b938bb94b6e827f959b98d8c773926
Codewithdean <142483331+Codewithdean@users.noreply.github.com>
2023-08-21 12:00:23 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<=1);  // this type of conversion is easy conversion.
+
+console.log("2">1);
+console.log(1<"2");
+
+console.log(null>0);
+console.log(null==0);   // To avoid this type of comparison
+console.log(null>=0);   // bcz there are lots of confusin in this type of comparison
+console.log(null<0);
+
+console.log(undefined>0);
+console.log(undefined>=0);  // To avoid this type of comparison
+console.log(undefined==0);
+console.log(undefined<0);
+
+// ===  this is strict type of operator. this type of operator to check the datatypes of the number.
+
+console.log("2" === 2);  // to check the datatypes of the number. 
+    

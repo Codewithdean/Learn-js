db3b743cd9e6b4af4b3dd387ca66e41158e43ab7
Codewithdean <142483331+Codewithdean@users.noreply.github.com>
2023-08-21 11:38:01 +0000

    perform operations

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,36 @@
+let value=3
+let negValue=-value
+console.log(value+negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1="Hii"
+let str2 = " Aman"
+let str3= str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("2" + 1 +1);
+console.log(1 + 1 + "2");
+console.log("2" + "1");
+
+//console.log(2+3*4%5);  //  good for question but not use in real life applications.
+
+console.log(true);
+console.log(+true);    // only for the knowledge.
+console.log(+"");     // this is not for the industry purpose.
+
+ // coding means good readability.
+
+ let gameCounter=100;
+ gameCounter++;        // use ++gameCounter;
+ console.log(gameCounter);  
+
+   //please read the prefix and postfix concept through MDN Docs.

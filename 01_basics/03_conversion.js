dc34994a1900e39ec824cc0a08eecfa82c5d6bed
Codewithdean <142483331+Codewithdean@users.noreply.github.com>
2023-08-21 09:56:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=33
+//"33abc" = NaN
+//Null = 0
+// "Aman" = NaN
+// true = 1 / false = 0
+
+let isLoggedIn="1"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "" = false
+// "Aman" = true
+
+let someNumber = "1234"
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
